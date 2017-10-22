eadab13df6952ffda2e10c872bff7505d1465c82
anssim <32455247+anssim@users.noreply.github.com>
2017-10-22 18:03:44 +0300

    comments added & cleanup

--- CServer/GraphicalUserInterface.py
+++ CServer/GraphicalUserInterface.py
@@ -17,14 +17,13 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 from kivy.input.motionevent import MotionEvent
 
-
+# Set up background image and properties
 class MyBackground(Widget):
 
     def __init__(self, **kwargs):
         super(MyBackground, self).__init__(**kwargs)
         with self.canvas:
             self.bg = Rectangle(source='project.jpg', pos=self.pos, size=self.size)
-            #self.add_widget(TextInput(text="HELLO WORLD", focus=True))
 
         self.bind(pos=self.update_bg)
         self.bind(size=self.update_bg)
@@ -40,35 +39,23 @@ class MyBackground(Widget):
         self.bg.pos = self.pos
         self.bg.size = self.size
 
-    def save(self):
-        pass
-
-    def load(self):
-        pass
-
+# Define contents of graphical node
 class ContentClass(GridLayout, Widget):
     def __init__(self, **kwargs):
         super(ContentClass, self).__init__(**kwargs)
         self.cols = 1
-
         self.slave = kwargs["slave"]
 
         self.nodemover = NodeMover(slave=self.slave, background_color=(0.8,0.8,0.8,0), text = "light", width = 1)
         self.textinput = TextInput(text="conf;g;g", background_color=(0.8,0.8,0.8,0.1), height=self.slave.size[1], width=100)
-        #self.send_button = Button(text="Send", background_color=(1,1,1,0.1), color=(0,0,0,1))
 
         self.add_widget(self.textinput)
         self.add_widget(self.nodemover)
-        #self.add_widget(self.send_button)
-
-        #def send_button_pressed(instance):
-        #    print "presseed"
-
-        #self.send_button.bind(on_press=send_button_pressed)
 
     def get_text(self):
         return self.textinput.text
 
+# Move node image
 class NodeMover(Image):
     def __init__(self, **kwargs):
         super(NodeMover, self).__init__(**kwargs)
@@ -83,6 +70,7 @@ class NodeMover(Image):
     def on_touch_up(self, touch):
         self.slave.on_touch_up_dummy(touch)
 
+# Create graphical version of node
 class GraphicalNode(GridLayout, ButtonBehavior, Image, Widget):
 
     def __init__(self, **kwargs):
@@ -93,7 +81,7 @@ class GraphicalNode(GridLayout, ButtonBehavior, Image, Widget):
         self.center_x = 500
         self.add_widget(self.txt)
 
-    #def on_touch_down(self, touch):
+    # Activate node
     def on_touch_down_dummy(self, touch):
         #return
         if not self.parent.get_grapped() is None:
@@ -104,6 +92,7 @@ class GraphicalNode(GridLayout, ButtonBehavior, Image, Widget):
         if touch.y < self.pos[1] or touch.y > self.pos[1] + self.size[1]:
             return
 
+        # Send configure if double click
         if touch.is_double_tap:
             # should there be other way to set conf besides sending the conf, no need at least as of yet
             self.node.set_conf(self.txt.get_text())
@@ -111,20 +100,21 @@ class GraphicalNode(GridLayout, ButtonBehavior, Image, Widget):
 
         self.parent.set_grapped(self)
 
-    #def on_touch_move(self, touch):
+    # Move node
     def on_touch_move_dummy(self, touch):
 
         if self.parent.get_grapped() is self:
             self.center_x = touch.x
             self.center_y = touch.y
 
-    #def on_touch_up(self, touch):
+    # Let go of node
     def on_touch_up_dummy(self, touch):
 
         if self.parent.get_grapped() is self:
             touch.ungrab(self)
             self.parent.set_grapped(None)
 
+    # Return node
     def get_node(self):
         return self.node
 
@@ -136,7 +126,7 @@ class GraphicalUserInterface(App):
         GraphicalNode(source="light.gif", center_x=150, center_y=50, node=self)
         GraphicalNode(source="button.gif", center_x=150, center_y=50, node=self)
         GraphicalNode(source="unknown.gif", center_x=150, center_y=50, node=self)
-        #self.root.add_widget(gnode)
+
         return self.root
 
     def get_root(self):
