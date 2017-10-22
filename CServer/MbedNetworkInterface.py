be02f2afe8ba3215ebe38d0d1c4443e20d96aaf3
anssim <32455247+anssim@users.noreply.github.com>
2017-10-22 17:56:50 +0300

    comments added

--- CServer/MbedNetworkInterface.py
+++ CServer/MbedNetworkInterface.py
@@ -24,9 +24,11 @@ class Node:
         self.nodelist = nodelist
         self.conf = "conf;g;g"
 
+        # Create a graphical version of node
         self.gnode = GraphicalUserInterface.GraphicalNode(color=(1,1,1,0.4), source=node_mode +".gif", center_x=50, center_y=50, node=self)
         gnode_parent.add_widget(self.gnode)
 
+    # Return list of nodes
     def get_nodelist(self):
         return self.nodelist
 
@@ -46,33 +48,42 @@ class Node:
     def clear_masters():
         pass
 
+    # Return node address
     def get_addr(self, length=0):
         if length <= 0 or length > len(self.addr):
             return self.addr
 
         return self.addr[-1 - len(self.addr) : -1]
 
+    # Return node timestamp
     def get_timestamp(self):
         return self.timestamp
 
+    # Return node timestamp
     def set_timestamp(self, timestamp):
         self.timestamp = timestamp
 
+    # Return node mode ( "light", "button", "unknown" )
     def get_node_mode(self):
         return self.node_mode
 
+    # Node to active/inactive ( True, False )
     def set_is_running(self, is_running):
         self.is_running = is_running
 
+    # Return node state
     def get_is_running(self):
         return self.is_running
 
+    # Return node configuration
     def get_conf(self):
         return self.conf
 
+    # Set node configuration
     def set_conf(self, conf="conf;g;g"):
         self.conf = conf
 
+    # Send message to device
     def send(self, msg=None, addr=None):
         if msg is None: msg = self.conf
         if addr is None: addr = self.addr
@@ -87,9 +98,11 @@ class NodeList:
         self.nodes = []
         self.networkInterface = networkInterface
 
+    # Return network interface
     def get_networkInterface(self):
         return self.networkInterface
 
+    # Update nodelist
     def update(self, addr, node_mode, gnode_parent):
         timestamp = int(time.time())
         found = False
@@ -103,19 +116,22 @@ class NodeList:
                 found = True
                 break
         if not found:
+            # Add new node to list
             self.nodes.append(Node(timestamp, addr, node_mode, self, gnode_parent))
 
+    # Remove outdated nodes
     def removeOutdated(self):
-        # remove outdated nodes
         timestamp = int(time.time())
         for idx, node in self.nodes:
             if node.is_running:
                 if timestamp - node.get_timestamp() > OUDATE_TIME:
                     node.set_is_running(False)
 
+    # Print list of current nodes
     def show(self):
         print self.nodes
 
+    # Send message
     def send(self, sock, addr, nodes=None):
         if nodes is None:
             nodes = self.nodes
@@ -126,9 +142,11 @@ class NodeList:
     def toList():
         return ((node.get_addr(), node.get_node_mode()) for node in self.nodes)
 
+    # Return list of nodes
     def get_nodes():
         return self.nodes
 
+    # Return all nodes of a mode
     def get_nodes_by_mode(self, mode):
         for node in self.nodes:
             if node.get_node_mode() is mode:
@@ -159,47 +177,37 @@ class MbedNetworkInterface:
         self.sock.bind(('', UDP_PORT))
 
         group_bin = socket.inet_pton(addrinfo[0], addrinfo[4][0])
+        
         # Join group
-
         mreq = group_bin + struct.pack('@I', 0)
         self.sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
         print "connected"
 
-
+    # Send message (to nodes on list?)
     def send(self, addr, nodes=None):
         print "networkinterface send"
-        self.nodelist.send(self.sock, addr, nodes) # nodelis takes care if nodes in case nodes is None
+        self.nodelist.send(self.sock, addr, nodes) # nodelist takes care if nodes in case nodes is None
 
+    # Send message (to mesh?)
     def sendMessage(self, msg="is_running=False", addr=UDP_IP, port=UDP_PORT):
-
-#        print "sending message"
-#
-#        addrinfo = socket.getaddrinfo(UDP_IP, None)[0]
-#
-#        s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
-#
-#        # Set Time-to-live (optional)
-#        ttl_bin = struct.pack('@i', 10)
-#        if addrinfo[0] == socket.AF_INET: # IPv4
-#            s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
-#        else:
-#            s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
-
         print "sending: " + addr + " - " + msg
         try:
             self.sock.sendto(msg, (addr, port))
         except Exception as e: # TODO (better error handling and such)
             print e
 
+    # Handle advertisements from devices
     def serve(self, gnode_parent):
         print "serving"
+        # Receive data from device
         data, addr = self.sock.recvfrom(256)
         addr = addr[0]
         #addr = ipaddress.ip_address(addr.decode('unicode-escape')).exploded[-IP_LENGTH_USED:]
         print "received message:", data
         print "   sent by:", addr
 
+        # Update the node state
         if "advertise:" in data:
             node_mode = data[len("advertise:"):]
             self.nodelist.update(addr, node_mode, gnode_parent)
@@ -207,6 +215,7 @@ class MbedNetworkInterface:
             node_mode = "unknown"
             self.nodelist.update(addr, node_mode, gnode_parent)
 
+    # Return list of nodes
     def get_nodelist(self):
         return self.nodelist
 
